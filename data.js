d7ca48ad45463cce872d8b5dbb8fc409cba66db4
rohitmitte <141019374+rohitmitte@users.noreply.github.com>
2024-02-10 18:39:07 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: '12th Fail',
+        des: " Every fight begins with a failure! Braving against the odds, Manoj Kumar Sharma puts it all on the line to crack the nation's much sought-after exam: UPSC.",
+        image: 'images/12fail.png'
+    },
+    {
+        name: 'The Legend of Hanuman',
+        des: 'Setting his mighty foot on Lanka, Hanuman unleashes himself against Ravan. Watch the legendary hero and the demon king go head to head in this ultimate war.',
+        image: 'images/hanuman2.png'
+    },
+    {
+        name: 'M.S. Dhoni: The Untold Story',
+        des: 'A tell-all tale about the life and times of Indian cricketer, Mahendra Singh Dhoni; mapping his journey; from a ticket collector to a celebrated cricketer.',
+        image: 'images/dhoni1.png'
+    },
+    {
+        name: 'Joe',
+        des: 'Joe, who is reeling from a heartbreak by his college sweetheart, reluctantly enters an arranged marriage. What surprises await in this new chapter for him?',
+        image: 'images/joe2.png'
+    },
+    {
+        name: 'Vikram',
+        des: 'A covert investigation changes its course when special officer Amar encounters an unrelenting Vikram and his pursuit to take down a notorious drug cartel',
+        image: 'images/vikram.png'
+    }
+]
